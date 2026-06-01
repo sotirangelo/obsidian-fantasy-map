db18b8402c15c4391672c9b17ea0fcc33a078eb2
sotirangelo <sotiraggelos@gmail.com>
2026-06-01 17:40:12 +0300

    chore(eslint): add svelte plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,17 @@ import tseslint from "typescript-eslint";
 import globals from "globals";
 import obsidianmd from "eslint-plugin-obsidianmd";
 import eslintConfigPrettier from "eslint-config-prettier";
+import svelte from "eslint-plugin-svelte";
 
 export default defineConfig(
+  globalIgnores([
+    "node_modules",
+    "dist",
+    "main.js",
+    "eslint.config.mjs",
+    "esbuild.config.mjs",
+    "package.json",
+  ]),
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
@@ -24,16 +33,23 @@ export default defineConfig(
     rules: {
       "@typescript-eslint/no-non-null-assertion": "off",
       "@typescript-eslint/no-confusing-void-expression": "off",
+      "@typescript-eslint/no-deprecated": "off",
     },
   },
-  globalIgnores([
-    "node_modules",
-    "dist",
-    "main.js",
-    "eslint.config.mjs",
-    "esbuild.config.mjs",
-    "package.json",
-    "src/**/*.svelte",
-  ]),
+  svelte.configs.recommended,
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        // Enable typescript parsing for `.svelte` files.
+        extraFileExtensions: [".svelte"],
+        parser: tseslint.parser,
+      },
+    },
+    rules: {
+      "no-unused-vars": "off",
+    },
+  },
   eslintConfigPrettier,
 );
